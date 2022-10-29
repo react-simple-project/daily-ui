28775810932181bc6096bd35878d4b15587c772d
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-29 14:18:30 -0400

    finish basic UI layout

--- src/components/UI/sidebar/SideBar.js
+++ src/components/UI/sidebar/SideBar.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../../../App.css'
+import styles from './Accordion.module.css'
 import Accordion from './Accordion'
 export default function SideBar() {
   return (
-    <section className='sidebar'><Accordion /></section>
+    <section className={`sidebar ${styles.sidebar}`}>
+      <Accordion />
+      <footer className={styles['sidebar__footer']}>
+        <h5 className={styles['sidebar__footer--text']}>Projects will be added as completed.</h5>
+      </footer>
+    </section>
   )
 }
