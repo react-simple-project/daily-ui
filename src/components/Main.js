673356547d050b06fdffca2724ee5435d79cfe26
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-29 16:55:44 -0400

    finish project-001

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
+import Project1 from './daily-ui-projects/001/Project1'
+import Project2 from './daily-ui-projects/002/Project2';
 export default function Main() {
+  const [currentProject] = useState(1);
   return (
-    <main></main>
+    <main>
+      {currentProject === 1 && <Project1 />}
+      {currentProject === 2 && <Project2 />}
+    </main>
   )
 }
---
+++ src/components/daily-ui-projects/001/components/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Heading() {
+  return (
+    <React.Fragment>
+      <h1>Signup for an adventure!</h1>
+      <h3>Adventure awaits...</h3>
+    </React.Fragment>
+  )
+}
