ae76dcaeb84680931bcd1b5c0664421e44bb6591
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-30 00:08:17 -0400

    refactor, clean up functions

--- src/components/UI/sidebar/Accordion.js
+++ src/components/UI/sidebar/Accordion.js
@@ -3,7 +3,7 @@ import styles from './Accordion.module.css'
 
 export default function Accordion(props) {
   const [currValue, setValue] = useState(0);
-
+  const { onSelectProject } = props.onSelectProject
   const accordionData = [{
     title: 'Project 001',
     content: <img className={styles['accordion__content--img']} alt='project 001 thumbnail' src='https://github.com/tm-LBenson/daily-ui/blob/main/public/assets/project-001.png?raw=true' ></img>
@@ -14,35 +14,22 @@ export default function Accordion(props) {
   },
   ];
 
-  const handleOnClick = event => {
-
-    if (currValue === event.target.title) {
-      setValue(0);
-    } else {
-      setValue(event.target.title)
-    }
-
-
-
-  }
-  const clickLinkHandler = (event) => {
-    props.onSelectProject.onSelectProject(+event.target.alt.split(' ')[1])
-  }
   return (
 
     < div >
       <h3 id='project' className={styles['sidebar__heading']}>Select a project from the list:</h3>
-
       {
         accordionData.map(project => (
           <div key={project.title}>
             <div className={styles.accordion}>
               <div className={styles['accordion__item']}>
-                <div onClick={handleOnClick} title={+project.title.slice(-3)} className={styles['accordion__title']}>
+                <div onClick={event => setValue(event.target.title)} title={+project.title.slice(-3)} className={styles['accordion__title']}>
                   <div title={+project.title.slice(-3)}>{project.title}</div>
                   <div title={+project.title.slice(-3)}>{+currValue === +project.title.slice(-3) ? '-' : '+'}</div>
                 </div>
-                {+currValue === +project.title.slice(-3) && <div className={styles['accordion__content']}><a onClick={clickLinkHandler} href='#project'>{project.content}</a></div>}
+                {+currValue === +project.title.slice(-3) && <div className={styles['accordion__content']}><a onClick={
+                  event => onSelectProject(+event.target.alt.split(' ')[1])
+                } href='#project'>{project.content}</a></div>}
               </div>
             </div>
           </div>)
